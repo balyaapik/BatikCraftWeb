8411c4f7fdd53883c67abc16cbada83a452aa14e
Davinga Balya Rochmadi <balyarochmadie@gmail.com>
2026-07-19 13:28:24 +0700

    Add storage app configuration

---
+++ storage_config/apps.py
@@ -0,0 +1,7 @@
+from django.apps import AppConfig
+
+
+class StorageConfigConfig(AppConfig):
+    default_auto_field = "django.db.models.BigAutoField"
+    name = "storage_config"
+    verbose_name = "Penyimpanan BatikCraft"
